6aab0a7785719501b3289d26844eacdd959ac891
Zsolt Üveges <zsolt.uveges93@gmail.com>
2018-09-07 15:11:33 +0200

    Created a transaction model

---
+++ src/main/java/hu/codecool/sctestproject/sctestproject/model/Transaction.java
@@ -0,0 +1,59 @@
+package hu.codecool.sctestproject.sctestproject.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.time.LocalDateTime;
+
+@Entity
+public class Transaction {
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    private Long id;
+
+    private LocalDateTime dateTime;
+    private Long amount;
+    private Long balance;
+
+    public Transaction() {
+    }
+
+    public Transaction(LocalDateTime dateTime, Long amount, Long balance) {
+        this.dateTime = dateTime;
+        this.amount = amount;
+        this.balance = balance;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public LocalDateTime getDateTime() {
+        return dateTime;
+    }
+
+    public void setDateTime(LocalDateTime dateTime) {
+        this.dateTime = dateTime;
+    }
+
+    public Long getAmount() {
+        return amount;
+    }
+
+    public void setAmount(Long amount) {
+        this.amount = amount;
+    }
+
+    public Long getBalance() {
+        return balance;
+    }
+
+    public void setBalance(Long balance) {
+        this.balance = balance;
+    }
+}
