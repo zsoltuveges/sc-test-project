3ef892ce69eb907e9096afc26d00ce45044b32ba
Zsolt Üveges <zsolt.uveges93@gmail.com>
2018-09-07 14:35:27 +0200

    Rename User to UserModel

--- src/main/java/hu/codecool/sctestproject/sctestproject/model/UserModel.java
+++ src/main/java/hu/codecool/sctestproject/sctestproject/model/UserModel.java
@@ -1,16 +1,23 @@
 package hu.codecool.sctestproject.sctestproject.model;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 @Entity
-public class User {
+public class UserModel {
     @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
     private Long id;
     private String username;
     private Long amount;
 
-    public User() {
+    public UserModel(String username) {
+        this.username = username;
+    }
+
+    public UserModel() {
     }
 
     public Long getId() {
