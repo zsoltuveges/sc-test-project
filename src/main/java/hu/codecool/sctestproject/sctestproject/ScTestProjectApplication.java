69c6c3a55dba62008286fde0a7d8816c306c0605
Zsolt Üveges <zsolt.uveges93@gmail.com>
2018-09-07 13:41:07 +0200

    Init a new springboot project

---
+++ src/main/java/hu/codecool/sctestproject/sctestproject/ScTestProjectApplication.java
@@ -0,0 +1,12 @@
+package hu.codecool.sctestproject.sctestproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScTestProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScTestProjectApplication.class, args);
+	}
+}
---
+++ src/test/java/hu/codecool/sctestproject/sctestproject/ScTestProjectApplicationTests.java
@@ -0,0 +1,16 @@
+package hu.codecool.sctestproject.sctestproject;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ScTestProjectApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
